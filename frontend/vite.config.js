ae3024f864811aef538ea4effa5a082d0295ee5b
Mahesh7s <mokamahesh77@gmail.com>
2025-10-24 15:58:09 +0530

    Initial commit - FarmToKitchen full stack project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // This is important for SPA routing
+  base: '/', // This is important for SPA routing
   build: {
     outDir: 'dist',
     sourcemap: false // Disable sourcemaps for smaller build
